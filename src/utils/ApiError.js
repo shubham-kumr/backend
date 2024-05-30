64144c359d0ada545b60a756ddd7533fa3547fc6
Shubham Kumar <shubhamkr6758@gmail.com>
2024-05-30 23:11:11 +0530

    fix bugs

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,23 +1,23 @@
 class ApiError extends Error {
     constructor(
         statusCode,
-        maessage= "Something went wrong",
+        message = "Something went wrong",
         errors = [],
         stack = ""
-    ){
-        super(meassage)
-        this.statusCode = statusCode
-        this.data = null,
-        this.message = message
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
         this.success = false;
-        this.errors = errors
+        this.errors = errors;
 
-        if(stack) {
+        if (stack) {
             this.stack = stack;
         } else {
-            Error.captureStacktree(this, this.constructor)
+            Error.captureStackTrace(this, this.constructor);
         }
     }
 }
 
-export {ApiError}
+export { ApiError };
